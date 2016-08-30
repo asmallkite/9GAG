feffd575b125b7819bfd8ef2fa4bad8b3e76620d
lizheng <lixiaozheng.good@163.com>
2016-08-30 21:04:06 +0800

    add RefreshBtn in actionbar

--- app/src/main/java/com/lizheng/www/a9gag/MainActivity.java
+++ app/src/main/java/com/lizheng/www/a9gag/MainActivity.java
@@ -64,14 +64,17 @@ public class MainActivity extends AppCompatActivity
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                onBackPressed();
+                return true;
+            case R.id.action_settings:
+                //TODO:
+                return  true;
+            default:
+                return super.onOptionsItemSelected(item);
         }
-
-        return super.onOptionsItemSelected(item);
     }
 
     @SuppressWarnings("StatementWithEmptyBody")
